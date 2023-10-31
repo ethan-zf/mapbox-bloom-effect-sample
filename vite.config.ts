4a69768524647bbef0d985a42071713170661e1e
ethan <zhufeng_solo@163.com>
2023-10-31 15:25:59 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig(({ mode }) => {
+  let config = {};
+  if (mode === 'dev') {
+    config = {
+      base: './',
+      define: {
+        'process.env': {
+          NODE_ENV: mode,
+        },
+      },
+      server: {
+        port: 3001,
+        open: true,
+      },
+    };
+  } else if (mode === 'prod') {
+    config = {
+      build: {
+        lib: {
+          entry: path.resolve(__dirname, 'src/index.ts'),
+          name: 'GlowLine',
+          fileName: 'GlowLine',
+        },
+      },
+    };
+  }
+
+  // config.resolve = {
+  //   alias: {
+  //     '@': path.resolve(__dirname, 'src'),
+  //     '@examples': path.resolve(__dirname, './examples'),
+  //   },
+  // };
+  return config;
+});
